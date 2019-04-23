51d1ed1a82903069f9790702fa64c2a33496bc49
Ozan <ozanp_1994@hotmail.com>
2019-04-23 14:57:23 +0200

    deleted unessecary files

--- Media Literacy Game/Assets/TextMesh Pro/Sprites/PlayerControler.cs
+++ Media Literacy Game/Assets/TextMesh Pro/Sprites/PlayerControler.cs
@@ -14,6 +14,6 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0)
+        //if (Input.touchCount > 0)
     }
 }
