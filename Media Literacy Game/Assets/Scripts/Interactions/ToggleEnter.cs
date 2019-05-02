ecb34294354c66566b5642103d31d747aba1d0f2
Ozan <ozanp_1994@hotmail.com>
2019-05-02 04:38:49 +0200

    toggling render now working
    a static button is not working

---
+++ Media Literacy Game/Assets/Scripts/Interactions/ToggleEnter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleEnter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer render = gameObject.GetComponent<Renderer>();
+
+        Vector3 position = transform.position;
+        var player = GameObject.Find("player");
+
+        if (player.gameObject.transform.position.x > position.x -1 && player.gameObject.transform.position.x < position.x + 1)
+        {
+            render.enabled = true;
+        }
+        else
+        {
+            render.enabled = false;
+        }
+
+        Debug.Log("button pos" + position.x);
+        Debug.Log("player pos" + player.gameObject.transform.position.x);
+    }
+}
