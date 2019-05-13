14f7e9d4355e29565dd8ec3fb8e056d0b43fddaa
Waelalghazouli <wael.ghz@live.com>
2019-05-13 19:24:49 +0200

    Bug fixed

---
+++ Media Literacy Game/Assets/Scripts/Main/TraverScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TraverScene : MonoBehaviour
+{
+    public void SceneTraverser(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
